92d9f00ca0548396e61ecfe762ae498c40218437
Lufanin <orion.pax37@gmail.com>
2024-06-28 23:37:20 +0300

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -6,15 +6,13 @@ def count_calls():
 def string_info(string):
     count_calls()
     b =(len(string), string.upper(), string.lower())
-    count_calls()
     return(b)
 def is_contains(string, list_to_search):
     count_calls()
     for i in range(len(list_to_search)):
         if string.lower() in list_to_search[i].lower() or list_to_search[i] in string.lower():
             return True
-        else:
-            return False
+        return False
 
 
 print(string_info('Armstrong'))
